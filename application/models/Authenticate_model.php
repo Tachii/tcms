86a3e101e9940086fef2d9efa5b61b2bc4ad4701
Tachi <zaveruha007@gmail.com>
2015-05-15 17:17:56 +0300

    tetst

---
+++ application/models/Authenticate_model.php
@@ -0,0 +1,14 @@
+<?php
+    class Authenticate_model extends CI_Model {
+
+        public $title;
+        public $content;
+        public $date;
+
+        public function __construct()
+        {
+                // Call the CI_Model constructor
+                parent::__construct();
+        }
+	}
+?>
