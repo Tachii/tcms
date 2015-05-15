757ef6c34c68fe4a4b04bc06a6cab5435b3e5ba7
Tachi <zaveruha007@gmail.com>
2015-05-15 16:33:24 +0300

    broke everythink

--- application/models/article_model.php
+++ application/models/article_model.php
@@ -1,3 +1,11 @@
-<?php
-    phpinfo();
-?>
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Article_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
