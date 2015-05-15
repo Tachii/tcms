b21d7cb976e59407f6e0ead842f91bc2e15dfa1c
Tachi <zaveruha007@gmail.com>
2015-05-15 15:44:02 +0300

    broke everythink

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/thecms/';
+$config['base_url'] = 'tcms.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
