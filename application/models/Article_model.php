3d32cb23122ed717161ff26e33544aba89c30b30
Tachi <zaveruha007@gmail.com>
2015-05-19 18:54:44 +0300

    Get all articles Method

--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -17,18 +17,25 @@
 		 */
 		public function get_articles($order_by = null, $sort='DESC', $limit = null, $offset = 0)
 		{
-			$this->db->select('a.*,b.name as category_name, c.first_name, c.last_name');
-			$this->db->from('articles as a');
-			$this->db->join('categories AS b', 'b.id = a.category_id','left');
-			$this->db->join('users as c', 'c.id=a.user_id','left');
+			
+			$this->db->from('articles AS article');
+			$this->db->select('article.*,category.name as category_name, user.first_name, user.last_name');
+			$this->db->join('categories AS category', 'category.id = article.category_id','left');
+			$this->db->join('users AS user','user.id=article.user_id','left');
+			
+			
 			if($limit != null){
 				$this->db->limit($limit, $offset);
 			}
+			
 			if($order_by != null){
 				$this->db->order_by($order_by,$sort);
 			}
+			
 			$query = $this->db->get();
-			return $query->result();
+			$result = $query->result();
+			return $result;
+		
 		}
 	}
 ?>
