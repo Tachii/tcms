325758f2926fc6ac8db9a83f3eaee6d1b56f9f48
Tachi <zaveruha007@gmail.com>
2015-05-19 15:43:53 +0300

    creating Controllers

---
+++ application/controllers/articles.php
@@ -0,0 +1,4 @@
+<?php
+class Articles extends MY_Controller{
+	
+}
---
+++ application/core/TCMS_Controller.php
@@ -0,0 +1,7 @@
+<?php
+//Global Controller
+class TCMS_Controller extends  CI_Controller{
+	public function __construct(){
+		parent::__construct();
+	}
+}
