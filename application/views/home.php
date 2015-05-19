f46a85d9fc12d507e503d20c8e5fca730a6ef03f
Tachi <zaveruha007@gmail.com>
2015-05-19 20:32:37 +0300

    Home view improvements

--- application/views/home.php
+++ application/views/home.php
@@ -25,9 +25,10 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
+            <li role="presentation" ><a href="<?php echo base_url();?>">Home</a></li>
+			<?php foreach($navbar_items as $item): ?>
+            	<li role="presentation"><a href="#"><?php echo $item->name; ?></a></li>
+          	<?php endforeach ; ?>
           </ul>
         </nav>
         <h3 class="text-muted">Project name</h3>
