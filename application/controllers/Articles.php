787bd4fbdf70bdabd06c2ce41f7f9f3c903c2b0d
Tachi <zaveruha007@gmail.com>
2015-05-19 18:00:42 +0300

    Articles_model

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Articles extends CI_Controller {
+class Articles extends TCMS_Controller {
 	/**
 	 * Index Page for this controller.
 	 *
@@ -19,6 +19,6 @@ class Articles extends CI_Controller {
 	public function index()
 	{
 		//Get Articles
-		$data['articles'] = $this->Article_model->get_articles();
+		$data['articles'] = $this->Article_model->get_articles('id','DESC','10');
 	}
 }
--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -7,9 +7,20 @@
                 parent::__construct();
         }
 		
-		public function get_articles()
+		public function get_articles($order_by = null, $sort='DESC', $limit = null, $offset = 0)
 		{
-			echo 'test';
+			$this->db->select('a.*,b.name as category_name, c.first_name, c.last_name');
+			$this->db->from('articles as a');
+			$this->db->join('categories AS b', 'b.id = a.category_id','left');
+			$this->db->join('users as c', 'c.id=a.user_id','left');
+			if($limit != null){
+				$this->db->limit($limit, $offset);
+			}
+			if($order_by != null){
+				$this->db->order_by($order_by,$sort);
+			}
+			$query = $this->db->get();
+			return $query->result();
 		}
 	}
 ?>
