829a787de18feda4838289d4fef9f900ad7b6fe5
Tachi <zaveruha007@gmail.com>
2015-05-19 16:10:23 +0300

    changing default controller

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'tcms.herokuapp.com';
+$config['base_url'] = 'http://tcms.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $config['base_url'] = 'tcms.herokuapp.com';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
