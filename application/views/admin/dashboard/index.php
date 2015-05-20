aef8dcaa344a2268bfd9923aafe459650647e26e
Tachi <zaveruha007@gmail.com>
2015-05-20 19:53:52 +0300

    Displaying Data in Dashboard

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -17,11 +17,11 @@
               	<?php var_dump($articles); ?>
               	<?php foreach($articles as $article) : ?>
 	                <tr>
+                	  <td><?php echo $article->id;?></td>
 	                  <td><?php echo $article->title;?></td>
 	                  <td><?php echo $article->category_name;?></td>
 	                  <td><?php echo $article->username;?></td>
-	                  <td>dolor</td>
-	                  <td>sit</td>
+	                  <td><?php echo $article->created;?></td>
 	                  <td><a href="edit_article.html" class="btn btn-primary">Edit</a> <a href="edit_article.html" class="btn btn-warning">Unpublish</a> <a href="edit_article.html" class="btn btn-danger">Delete</a></td>
 	                </tr>
                 <?php endforeach; ?>
