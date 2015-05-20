2ef4918cd135f096b8086a9fa9976027ad883a40
Tachi <zaveruha007@gmail.com>
2015-05-20 17:13:13 +0300

    Dashboard Controller

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -6,5 +6,28 @@
                 // Call the CI_Model constructor
                 parent::__construct();
         }
+		
+		/**
+		 * Get All Users Method
+		 * 
+		 * @param - (string) $order_by 
+		 * @param - (string) $sort
+		 * @param - (int) $limit
+		 * @param - (int) $offset
+		 * 
+		 */
+		public function get_users($order_by=null, $sort='DESC', $limit = null, $offset=0){
+			$this->db->select('*');
+			$this->db->from('users');
+			if($limit != null){
+				$this->db->limit($limit, $offset);
+			}
+			if($order != null){
+				$this->db->order_by($order_by, $offset);
+			}
+			
+			$query=$this->db->get();
+			return $query->result();
+		}
 	}
 ?>
