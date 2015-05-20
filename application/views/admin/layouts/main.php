190acfecfcb0b1d6d18a4eeffd2d5d850fc8cb40
Tachi <zaveruha007@gmail.com>
2015-05-20 19:26:43 +0300

    Creatind Dashboard View Elements

--- application/views/admin/layouts/main.php
+++ application/views/admin/layouts/main.php
@@ -5,13 +5,5 @@
 			<!-- Loading Main Content -->
 			<?php $this->load->view($main_content); ?>
         </div>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
-  </body>
-</html>
+        
+<?php $this->load->view('admin/layouts/includes/footer'); ?>
