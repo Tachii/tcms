60daff7f23d02cc717d0748bf509030775e5afe4
Tachi <zaveruha007@gmail.com>
2015-05-20 18:25:21 +0300

    Admin Dashboard Layout

--- application/views/admin/layouts/main.php
+++ application/views/admin/layouts/main.php
@@ -35,7 +35,7 @@
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
             <li><a href="<?php echo base_url(); ?>">Dashboard</a></li>
-            <li><a href="#">Logout</a></li>
+            <li><a href="<?php echo base_url(); ?>admin/users/logout">Logout</a></li>
           </ul>
           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search Articles...">
