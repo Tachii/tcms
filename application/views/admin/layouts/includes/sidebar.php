f6c5e34f0f56e542b127f9f4ed512b897624c74c
Tachi <zaveruha007@gmail.com>
2015-05-21 23:57:28 +0300

    sidar improvements

--- application/views/admin/layouts/includes/sidebar.php
+++ application/views/admin/layouts/includes/sidebar.php
@@ -1,10 +1,10 @@
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
             <li <?php if(current_url()==base_url()."admin/dashboard"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/dashboard">Overview</a></li>
-            <li><a href="<?php echo base_url(); ?>admin/articles">Articles</a></li>
-            <li><a href="<?php echo base_url(); ?>admin/categories.html">Categories</a></li>
-            <li><a href="<?php echo base_url(); ?>admin/users">Users</a></li>
-            <li><a href="<?php echo base_url(); ?>admin/groups">User Groups</a></li>
-            <li><a href="<?php echo base_url(); ?>admin/settings">Settings</a></li>
+            <li <?php if(current_url()==base_url().">admin/articles"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/articles">Articles</a></li>
+            <li <?php if(current_url()==base_url().">admin/categories"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/categories">Categories</a></li>
+            <li <?php if(current_url()==base_url().">admin/users"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/users">Users</a></li>
+            <li <?php if(current_url()==base_url().">admin/groups"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/groups">User Groups</a></li>
+            <li <?php if(current_url()==base_url().">admin/settings"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/settings">Settings</a></li>
           </ul>
         </div>
