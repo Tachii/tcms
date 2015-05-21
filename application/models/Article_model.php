f670139ba1baeee2617dd1ff65427a6c330029c8
Tachi <zaveruha007@gmail.com>
2015-05-21 15:33:13 +0300

    Admin Articles Controller & View

--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -39,29 +39,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		}
 		
 		/**
-		 * Get Categories
-		 * 
-		 * @param - order_by(string)
-		 * @param - sort(string)
-		 * @param - limit(int)
-		 * @param - offset(int)
-		 * 
-		 */
-		public function get_categories($order_by = null, $sort='DESC', $limit = null, $offset = 0)
-		{
-			$this->db->select('*');
-			$this->db->from('categories');
-			if($limit != null){
-				$this->db->limit($limit,$offset);
-			}
-			if($order_by != null){
-				$this->db->order_by($order_by,$offset);
-			}
-			$query = $this->db->get();
-			return $query->result();
-		}
-		
-		/**
 		 * Get Navbar Items
 		 */
 		public function get_navbar_items()
