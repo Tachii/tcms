5ba30da6c676908783602141bf3f9161e4379b4b
Tachi <zaveruha007@gmail.com>
2015-05-21 23:54:36 +0300

    sidar improvements

--- application/views/admin/layouts/includes/sidebar.php
+++ application/views/admin/layouts/includes/sidebar.php
@@ -1,6 +1,6 @@
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li><a href="<?php echo base_url(); ?>admin/dashboard">Overview</a></li>
+            <li <?php if(current_url()==base_url()."admin/dashboard"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/dashboard">Overview</a></li>
             <li><a href="<?php echo base_url(); ?>admin/articles">Articles</a></li>
             <li><a href="<?php echo base_url(); ?>admin/categories.html">Categories</a></li>
             <li><a href="<?php echo base_url(); ?>admin/users">Users</a></li>
