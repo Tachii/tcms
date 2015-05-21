59576527b097ed2b0a04c0cb85bb6415b7dba199
Tachi <zaveruha007@gmail.com>
2015-05-21 15:41:44 +0300

    Admin Articles Controller & View

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -23,7 +23,7 @@ class Articles extends TCMS_Controller {
 		$data['articles'] = $this->Article_model->get_articles('id','DESC','10');
 		
 		//Get Categories
-		$data['categories'] = $this->Category_model->get_categories('id', 'DESC', 5);
+		$data['categories'] = $this->Categories_model->get_categories('id', 'DESC', 5);
 		
 		//Get Users
 		$data['users'] = $this->User_model->get_users('id', 'DESC', 5);
--- application/models/Categories_model.php
+++ application/models/Categories_model.php
@@ -1,47 +1,36 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-    class Categories_model extends CI_Model {
-    	
-        public function __construct()
-        {
-                // Call the CI_Model constructor
-                parent::__construct();
-        }
-		
-		/**
-		 * Get Categories
-		 * 
-		 * @param - order_by(string)
-		 * @param - sort(string)
-		 * @param - limit(int)
-		 * @param - offset(int)
-		 * 
-		 */
-		public function get_categories($order_by = null, $sort='DESC', $limit = null, $offset = 0)
-		{
-			$this->db->select('*');
-			$this->db->from('categories');
-			if($limit != null){
-				$this->db->limit($limit,$offset);
-			}
-			if($order_by != null){
-				$this->db->order_by($order_by,$offset);
-			}
-			$query = $this->db->get();
-			return $query->result();
+class Categories_model extends CI_Model {
+	
+    public function __construct()
+    {
+            // Call the CI_Model constructor
+            parent::__construct();
+    }
+	
+	/**
+	 * Get Categories
+	 * 
+	 * @param - order_by(string)
+	 * @param - sort(string)
+	 * @param - limit(int)
+	 * @param - offset(int)
+	 * 
+	 */
+	public function get_categories($order_by = null, $sort='DESC', $limit = null, $offset = 0)
+	{
+		$this->db->select('*');
+		$this->db->from('categories');
+		if($limit != null){
+			$this->db->limit($limit,$offset);
 		}
-		
-		/**
-		 * Get Navbar Items
-		 */
-		public function get_navbar_items()
-		{
-			$this->db->where('in_navbar',1);
-			$this->db->order_by('order');
-			$query=$this->db->get('articles'); 
-			$navbar_items = $query->result();
-			return $navbar_items;
+		if($order_by != null){
+			$this->db->order_by($order_by,$offset);
 		}
-		
+		$query = $this->db->get();
+		$categories = $query->result();
+		return $categories;
 	}
+	
+}
 ?>
