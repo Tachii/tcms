5f7481730eefc1136fc3013b0f78a6a1eab39b1c
Tachi <zaveruha007@gmail.com>
2015-05-22 14:50:39 +0300

    Article Update View

--- application/controllers/admin/Articles.php
+++ application/controllers/admin/Articles.php
@@ -102,30 +102,34 @@ class Articles extends TCMS_Controller {
 		$data['groups'] = $this->Groups_model->get_groups();
 		$data['article'] = $this->Article_model->get_single_article($id);
 		
-		if($this->form_validation->run() == FALSE){
-			//Views
-			$data['main_content'] = 'admin/articles/edit';
-			$this->load->view('admin/layouts/main',$data);
+		if(!empty($data['article'])){
+			if($this->form_validation->run() == FALSE){
+				//Views
+				$data['main_content'] = 'admin/articles/edit';
+				$this->load->view('admin/layouts/main',$data);
+			} else {
+				//Create Articles Data Array
+				$data = array(
+					'title' 			=> $this->input->post('title'),
+					'body' 				=> $this->input->post('body'),
+					'category_id' 		=> $this->input->post('category_id'),
+					'user_id' 			=> $this->input->post('user_id'),
+					'access' 			=> $this->input->post('access'),
+					'is_published' 		=> $this->input->post('is_published'),
+					'in_navbar' 		=> $this->input->post('in_navbar'),
+					'order' 			=> $this->input->post('order'),
+				);
+							
+				//Insert into Articles Table
+				$this->Article_model->update($data, $id);
+				
+				//Create Notification
+				$this->session->set_flashdata('article_saved','Your article was saved!');
+				
+				//Redirect
+				redirect('admin/articles');
+			}
 		} else {
-			//Create Articles Data Array
-			$data = array(
-				'title' 			=> $this->input->post('title'),
-				'body' 				=> $this->input->post('body'),
-				'category_id' 		=> $this->input->post('category_id'),
-				'user_id' 			=> $this->input->post('user_id'),
-				'access' 			=> $this->input->post('access'),
-				'is_published' 		=> $this->input->post('is_published'),
-				'in_navbar' 		=> $this->input->post('in_navbar'),
-				'order' 			=> $this->input->post('order'),
-			);
-						
-			//Insert into Articles Table
-			$this->Article_model->update($data, $id);
-			
-			//Create Notification
-			$this->session->set_flashdata('article_saved','Your article was saved!');
-			
-			//Redirect
 			redirect('admin/articles');
 		}
 		
