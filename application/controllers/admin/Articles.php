ad32818cc6761b5333916594e1617718f6d1fb3c
Tachi <zaveruha007@gmail.com>
2015-05-22 14:36:27 +0300

    Article Update View

--- application/controllers/admin/Articles.php
+++ application/controllers/admin/Articles.php
@@ -87,7 +87,10 @@ class Articles extends TCMS_Controller {
 	 * Edit Article method
 	 * @param id(int)
 	 */
-	public function edit($id=null){
+	public function edit($id){
+		if(empty($id)){
+			redirect('admin/articles');
+		}
 		//Form Validation Rules
 		$this->form_validation->set_rules('title','Title','trim|required|min_length[4]|xss_clean');
 		$this->form_validation->set_rules('body','Body','trim|required|xss_clean');
