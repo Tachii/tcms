865f715e93e3ec5531ec36e7892fdd286270e5cd
Tachi <zaveruha007@gmail.com>
2015-05-25 20:17:01 +0300

    Sidebar improvements

--- application/views/admin/layouts/includes/sidebar.php
+++ application/views/admin/layouts/includes/sidebar.php
@@ -1,10 +1,10 @@
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li <?php if(current_url()==base_url()."admin/dashboard"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/dashboard">Overview</a></li>
-            <li <?php if(current_url()==base_url()."admin/articles"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/articles">Articles</a></li>
-            <li <?php if(current_url()==base_url()."admin/categories"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/categories">Categories</a></li>
-            <li <?php if(current_url()==base_url()."admin/users"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/users">Users</a></li>
-            <li <?php if(current_url()==base_url()."admin/groups"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/groups">User Groups</a></li>
-            <li <?php if(current_url()==base_url()."admin/settings"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/settings">Settings</a></li>
+            <li <?php if($this->uri->segment(2) == "dashboard"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/dashboard">Overview</a></li>
+            <li <?php if($this->uri->segment(2) == "articles"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/articles">Articles</a></li>
+            <li <?php if($this->uri->segment(2) == "categories"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/categories">Categories</a></li>
+            <li <?php if($this->uri->segment(2) == "users"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/users">Users</a></li>
+            <li <?php if($this->uri->segment(2) == "groups"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/groups">User Groups</a></li>
+            <li <?php if($this->uri->segment(2) == "settings"){echo 'class="active"';} ?>><a href="<?php echo base_url(); ?>admin/settings">Settings</a></li>
           </ul>
         </div>
