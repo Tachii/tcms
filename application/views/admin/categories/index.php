ac77d5d87e56952264419a4249339eafca435738
Tachi <zaveruha007@gmail.com>
2015-05-25 14:15:17 +0300

    Categories Model & Controller

--- application/views/admin/categories/index.php
+++ application/views/admin/categories/index.php
@@ -1,3 +1,18 @@
+<!-- Display Notification Message -->
+<?php
+	if($this->session->flashdata('category_saved')){
+		echo ('<p class="alert alert-success">'.$this->session->flashdata('category_saved')."</p>");
+	}
+	if($this->session->flashdata('category_saved_error')){
+		echo ('<p class="alert alert-danger">'.$this->session->flashdata('category_saved_error')."</p>");
+	}
+	if($this->session->flashdata('category_deleted')){
+		echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted')."</p>";
+	}
+	if($this->session->flashdata('category_deleted_error')){
+		echo '<p class="alert alert-danger">'.$this->session->flashdata('category_deleted_error')."</p>";
+	}
+?>
 <h1 class="sub-header">Categories</h2>
       	  <a href="<?php echo base_url() ?>admin/categories/add" class="btn btn-success pull-right">Add Category</a><br /><br />
           <div class="table-responsive">
