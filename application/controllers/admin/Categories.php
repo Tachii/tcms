7275ff9a3861e5f494357428e0127680f5815f98
Tachi <zaveruha007@gmail.com>
2015-05-25 17:55:34 +0300

    Categories Model & Controller

--- application/controllers/admin/Categories.php
+++ application/controllers/admin/Categories.php
@@ -14,6 +14,37 @@ class Categories extends TCMS_Controller{
 	 * Add Category
 	 */
 	public function add(){
+		//Validation Rules
+		$this->form_validation->set_rules('name','Name','trim|required|min_length[4]|xss_clean');
+		
+		//Checking if form was validated
+		if($this->form_validation->run() === FALSE){
+			//Views
+			$data['main_content'] = 'admin/articles/add';
+			$this->load->view('admin/layouts/main',$data);
+		} else {
+			//Set data
+			$data =  $this->input->post('name');
+			
+			//Insert data into Categories Table
+			if($this->Categories_model->insert($data)){
+				
+				//Create Notification
+				$this->session->set_flashdata('category_saved','Your category was saved!');
+				
+				//Redirect
+				redirect('admin/categories');
+			} else {
+				//Create Notification
+				$this->session->set_flashdata('category_saved_error','Your category was not saved!');
+				
+				//Redirect
+				redirect('admin/categories');
+			}
+			
+			
+		}
+		
 		//Loading Views
 		$data['main_content'] = 'admin/categories/add';
 		$this->load->view('admin/layouts/main',$data);
