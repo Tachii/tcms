c824fd45bc30c253738573945ae558385d1859e0
Tachi <zaveruha007@gmail.com>
2015-05-25 13:36:13 +0300

    Categories Model & Controller

--- application/controllers/admin/Categories.php
+++ application/controllers/admin/Categories.php
@@ -1,3 +1,12 @@
 <?php
-    phpinfo();
-?>
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Categories extends TCMS_Controller{
+	public function index(){
+		//Get Categoires
+		$data['categories'] = $this->Categories_model->get_categories('id','DESC');
+		
+		//Loading Views
+		$data['main_content'] = 'admin/categories/index';
+		$this->load->view('admin/layouts/main',$data);
+	}
+}
