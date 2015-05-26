62fd1b2d7afae4aa83ae9410ff3c862d56efaf79
Tachi <zaveruha007@gmail.com>
2015-05-26 17:27:58 +0300

    Users Model,Controller,View

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -17,10 +17,9 @@ class User_model extends CI_Model {
 	 * 
 	 */
 	public function get_users($order_by=null, $sort='DESC', $limit = null, $offset=0){
-		$this->db->select('users.*,group.name AS group_name');
+		$this->db->select('users.id, first_name, last_name, username, email, password,group.name AS group_name');
 		$this->db->from('users');
-		$this->db->join('groups','groups.id = users.group_id');
-		
+		$this->db->join('groups', 'groups.id=users.group_id', 'INNER');
 		
 		if($limit != null){
 			$this->db->limit($limit, $offset);
