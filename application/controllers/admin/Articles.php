7f1b91adffc062897019f6bba5070471a6095f5d
Tachi <zaveruha007@gmail.com>
2015-05-26 14:56:38 +0300

    Search implementation

--- application/controllers/admin/Articles.php
+++ application/controllers/admin/Articles.php
@@ -4,7 +4,7 @@ class Articles extends TCMS_Controller {
 	public function index(){
 		if(!empty($this->input->post('keywords'))){
 			//Get filtered Articles
-			$data['articles'] = $this->Article_model->get_filtered_articles('id','DESC',10);
+			$data['articles'] = $this->Article_model->get_filtered_articles($this->input->post('keywords'));
 		} else {
 			//Get just all the articles(or limited number)
 			$data['articles'] = $this->Article_model->get_articles('id','DESC',10);
