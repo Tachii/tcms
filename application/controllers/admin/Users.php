d5a1d17761cfafa05a587fb7acda8896b8aa7a71
Tachi <zaveruha007@gmail.com>
2015-05-26 15:41:47 +0300

    Users Model,Controller,View

--- application/controllers/admin/Users.php
+++ application/controllers/admin/Users.php
@@ -1,2 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+class Users extends TCMS_Controller {
+	public function index(){
+		//Load View
+		$data['main_content'] = 'admin/users/index';
+		$this->load->view('admin/layouts/main',$data);
+	}
+}
