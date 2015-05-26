c49f5c42c4f2fbf6f55a0a2feb44aecf49ff307b
Tachi <zaveruha007@gmail.com>
2015-05-26 15:06:51 +0300

    Users Model,Controller,View

--- application/controllers/admin/Articles.php
+++ application/controllers/admin/Articles.php
@@ -4,7 +4,9 @@ class Articles extends TCMS_Controller {
 	public function index(){
 		if(!empty($this->input->post('keywords'))){
 			//Get filtered Articles
+			die();
 			$data['articles'] = $this->Article_model->get_filtered_articles($this->input->post('keywords'));
+			
 		} else {
 			//Get just all the articles(or limited number)
 			$data['articles'] = $this->Article_model->get_articles('id','DESC',10);
