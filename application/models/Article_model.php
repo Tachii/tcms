27aa67136397b1bf76c110384619df05b0b42494
Tachi <zaveruha007@gmail.com>
2015-05-26 15:01:46 +0300

    Users Model,Controller,View

--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -32,11 +32,33 @@ class Article_model extends CI_Model {
 	
 	/**
 	 * Get Filtered Articles
+	 * @param - keywords(string)
 	 * @param - order_by(string)
 	 * @param - sort(string)
 	 * @param - limit(int)
 	 * @param - offset(int)
 	 */
+	public function get_filtered_articles($keywords, $order_by = null, $sort = 'DESC', $limit = null, $offset = 0){
+		$this->db->from('articles AS article');
+		$this->db->select('article.*,category.name as category_name, user.first_name, user.last_name, user.username');
+		$this->db->join('categories AS category', 'category.id = article.category_id','left');
+		$this->db->join('users AS user','user.id=article.user_id','left');
+		$this->db->like('title',$keywords);
+		$this->db->or_like('body',$keywords);
+		
+		
+		if($limit != null){
+			$this->db->limit($limit, $offset);
+		}
+		
+		if($order_by != null){
+			$this->db->order_by($order_by,$sort);
+		}
+		
+		$query = $this->db->get();
+		$articles = $query->result();
+		return $articles;
+	}
 	
 	/**
 	 * Get Navbar Items
