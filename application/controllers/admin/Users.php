01318387e44e1ddb225b2f2c4f7f0332abe1c45e
Tachi <zaveruha007@gmail.com>
2015-05-26 16:13:33 +0300

    Users Model,Controller,View

--- application/controllers/admin/Users.php
+++ application/controllers/admin/Users.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends TCMS_Controller {
 	public function index(){
 		//Getting data
-		$data['groups'] = $this->Groups_model->get_categories();
+		$data['groups'] = $this->Groups_model->get_groups();
 		
 		//Load View
 		$data['main_content'] = 'admin/users/index';
