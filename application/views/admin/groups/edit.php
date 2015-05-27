edcb9d356aef04c97422be70385f9e8003b0ecea
Tachi <zaveruha007@gmail.com>
2015-05-27 15:54:06 +0300

    Groups MVC

--- application/views/admin/groups/edit.php
+++ application/views/admin/groups/edit.php
@@ -1,6 +1,6 @@
 <!-- Display Validation errors -->
 <?php echo validation_errors('<p class="alert alert-dismissable alert-danger">'); ?>
-<form method="post" action="<?php echo base_url() ?>admin/groups/edit/<?php echo $category->id ?>"?>>
+<form method="post" action="<?php echo base_url() ?>admin/groups/edit/<?php echo $group->id ?>"?>>
 	<div class="row">	
 	  <div class="col-md-6">
 	  	<h1 class="sub-header">Edit Group</h1>
@@ -14,16 +14,16 @@
 				</li>
 				<li>
 					<a href="<?php echo base_url();?>admin/groups">
-						Categories
+						groups
 					</a>
 				</li>
 				<li>
-					Add Category
+					Add group
 				</li>
 			</ol>
 			
 			<div class="form-group col-xs-3">
-				<label>Category Name</label>
+				<label>group Name</label>
 				<input type="text" class="form-control" name="name" value="<?php echo $group->name; ?>" placeholder="Enter Group Name" />
 				<br />
 				<div class="btn-group pull-left">
