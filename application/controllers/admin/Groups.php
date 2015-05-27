85cdf268c67f0ffc4a0662b4723989bb3390502c
Tachi <zaveruha007@gmail.com>
2015-05-27 15:21:35 +0300

    Groups MVC

---
+++ application/controllers/admin/Groups.php
@@ -0,0 +1,116 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Groups extends TCMS_Controller{
+	public function index(){
+		//Get Categoires
+		$data['groups'] = $this->Groups_model->get_groups('id','DESC');
+		
+		//Loading Views
+		$data['main_content'] = 'admin/groups/index';
+		$this->load->view('admin/layouts/main',$data);
+	}
+	
+	/**
+	 * Add Category
+	 */
+	public function add(){
+		//Validation Rules
+		$this->form_validation->set_rules('name','Category','trim|required|min_length[4]|xss_clean');
+		
+		//Checking if form was validated
+		if($this->form_validation->run() === FALSE){
+			//Views
+			$data['main_content'] = 'admin/categories/add';
+			$this->load->view('admin/layouts/main',$data);
+		} else {
+			//Set data
+			$data =  array('name' => $this->input->post('name'));
+			
+			//Insert data into Categories Table
+			if($this->Categories_model->insert($data)){
+				
+				//Create Notification
+				$this->session->set_flashdata('category_saved','Your category was saved!');
+				
+				//Redirect
+				redirect('admin/categories');
+			} else {
+				//Create Notification
+				$this->session->set_flashdata('category_saved_error','Your category was not saved!');
+				
+				//Redirect
+				redirect('admin/categories');
+			}
+		}
+	}
+	
+	/**
+	 * Edit Category
+	 * @param - id(int)
+	 */
+	public function edit($id){
+		if(empty($id)){
+			$this->session->set_flashdata('category_saved_error','Category with such id doesn\'t exist');
+			redirect('admin/categories');
+		}
+		
+		//To display data in form
+		$data['category'] = $this->Categories_model->get_single_category($id);
+		
+		//Validation Rules
+		$this->form_validation->set_rules('name','Category','trim|required|min_length[4]|xss_clean');
+		
+		if(!empty($data['category'])){
+			//Checking if form was validated
+			if($this->form_validation->run() === FALSE){
+				//Views
+				$data['main_content'] = 'admin/categories/edit';
+				$this->load->view('admin/layouts/main',$data);
+			} else {
+				//Set data
+				$data =  array('name' => $this->input->post('name'));
+				
+				//Insert data into Categories Table
+				if($this->Categories_model->update($id,$data)){
+					
+					//Create Notification
+					$this->session->set_flashdata('category_saved','Your category was saved!');
+					
+					//Redirect
+					redirect('admin/categories');
+				} else {
+					//Create Notification
+					$this->session->set_flashdata('category_saved_error','Your category was not saved!');
+					
+					//Redirect
+					redirect('admin/categories');
+				}
+			}
+		} else {
+			//Redirect
+			redirect('admin/categories');
+		}
+	
+	}
+
+	/**
+	 * Delete Category
+	 * @param - id(int);
+	 */
+	public function delete($id){
+		
+		if($this->Categories_model->delete($id)){
+			//Create Message & Redirect
+			$this->session->set_flashdata('category_deleted', 'Category has been deleted');
+			redirect('admin/categories');
+		} else{
+			//Create Message & Redirect
+			$this->session->set_flashdata('category_deleted_error', 'Category has not been deleted');
+			redirect('admin/categories');
+		}
+		
+		
+		
+	}
+	
+}
