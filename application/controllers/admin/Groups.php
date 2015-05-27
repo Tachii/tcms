c7df1da82fcfb17c4636e202da8c458c67f91845
Tachi <zaveruha007@gmail.com>
2015-05-27 15:25:41 +0300

    Groups MVC

--- application/controllers/admin/Groups.php
+++ application/controllers/admin/Groups.php
@@ -11,102 +11,102 @@ class Groups extends TCMS_Controller{
 	}
 	
 	/**
-	 * Add Category
+	 * Add group
 	 */
 	public function add(){
 		//Validation Rules
-		$this->form_validation->set_rules('name','Category','trim|required|min_length[4]|xss_clean');
+		$this->form_validation->set_rules('name','Groups','trim|required|min_length[4]|xss_clean');
 		
 		//Checking if form was validated
 		if($this->form_validation->run() === FALSE){
 			//Views
-			$data['main_content'] = 'admin/categories/add';
+			$data['main_content'] = 'admin/groups/add';
 			$this->load->view('admin/layouts/main',$data);
 		} else {
 			//Set data
 			$data =  array('name' => $this->input->post('name'));
 			
-			//Insert data into Categories Table
-			if($this->Categories_model->insert($data)){
+			//Insert data into groups Table
+			if($this->Groups_model->insert($data)){
 				
 				//Create Notification
-				$this->session->set_flashdata('category_saved','Your category was saved!');
+				$this->session->set_flashdata('group_saved','New Group Was Added');
 				
 				//Redirect
-				redirect('admin/categories');
+				redirect('admin/groups');
 			} else {
 				//Create Notification
-				$this->session->set_flashdata('category_saved_error','Your category was not saved!');
+				$this->session->set_flashdata('group_saved_error','Group was not saved');
 				
 				//Redirect
-				redirect('admin/categories');
+				redirect('admin/groups');
 			}
 		}
 	}
 	
 	/**
-	 * Edit Category
+	 * Edit group
 	 * @param - id(int)
 	 */
 	public function edit($id){
 		if(empty($id)){
-			$this->session->set_flashdata('category_saved_error','Category with such id doesn\'t exist');
-			redirect('admin/categories');
+			$this->session->set_flashdata('group_saved_error','group with such id doesn\'t exist');
+			redirect('admin/groups');
 		}
 		
 		//To display data in form
-		$data['category'] = $this->Categories_model->get_single_category($id);
+		$data['group'] = $this->Groups_model->get_single_group($id);
 		
 		//Validation Rules
-		$this->form_validation->set_rules('name','Category','trim|required|min_length[4]|xss_clean');
+		$this->form_validation->set_rules('name','group','trim|required|min_length[4]|xss_clean');
 		
-		if(!empty($data['category'])){
+		if(!empty($data['group'])){
 			//Checking if form was validated
 			if($this->form_validation->run() === FALSE){
 				//Views
-				$data['main_content'] = 'admin/categories/edit';
+				$data['main_content'] = 'admin/groups/edit';
 				$this->load->view('admin/layouts/main',$data);
 			} else {
 				//Set data
 				$data =  array('name' => $this->input->post('name'));
 				
-				//Insert data into Categories Table
-				if($this->Categories_model->update($id,$data)){
+				//Insert data into groups Table
+				if($this->Groups_model->update($id,$data)){
 					
 					//Create Notification
-					$this->session->set_flashdata('category_saved','Your category was saved!');
+					$this->session->set_flashdata('group_saved','Group was saved!');
 					
 					//Redirect
-					redirect('admin/categories');
+					redirect('admin/groups');
 				} else {
 					//Create Notification
-					$this->session->set_flashdata('category_saved_error','Your category was not saved!');
+					$this->session->set_flashdata('group_saved_error','Group was not saved!');
 					
 					//Redirect
-					redirect('admin/categories');
+					redirect('admin/groups');
 				}
 			}
 		} else {
 			//Redirect
-			redirect('admin/categories');
+			redirect('admin/groups');
 		}
 	
 	}
 
 	/**
-	 * Delete Category
+	 * Delete group
 	 * @param - id(int);
 	 */
 	public function delete($id){
 		
-		if($this->Categories_model->delete($id)){
+		if($this->Groups_model->delete($id)){
 			//Create Message & Redirect
-			$this->session->set_flashdata('category_deleted', 'Category has been deleted');
-			redirect('admin/categories');
+			$this->session->set_flashdata('group_deleted', 'Group has been deleted');
+			redirect('admin/groups');
 		} else{
 			//Create Message & Redirect
-			$this->session->set_flashdata('category_deleted_error', 'Category has not been deleted');
-			redirect('admin/categories');
+			$this->session->set_flashdata('group_deleted_error', 'Group has not been deleted');
+			redirect('admin/groups');
 		}
 		
 		
