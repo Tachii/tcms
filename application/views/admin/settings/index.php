8662a6ca3c74bb14f5427e850571c7456b543064
Tachi <zaveruha007@gmail.com>
2015-05-28 17:29:05 +0300

    Settings MVC

--- application/views/admin/settings/index.php
+++ application/views/admin/settings/index.php
@@ -1,3 +1,29 @@
-<?php
-    phpinfo();
-?>
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<form>
+    	<div class="row">	
+	      <div class="col-md-6">
+	      	<h1 class="sub-header">Settings</h1>
+	      </div>
+    	</div>
+    			<ol class="breadcrumb">
+    				<li>
+    					<a href="#">
+    						Dashboard
+    					</a>
+    				</li>
+    				<li>
+						Settings
+    				</li>
+    			</ol>
+    		<div class="form-group">
+    			<label>Jumbotron Title</label>
+    			<input type="text" class="form-control" name="jtitle" />
+    			<label>Jumbotron Text</label>
+    			<textarea class="form-control" name="jtext" rows="10"> </textarea>
+    		</div>
+	      	<div class="btn-group pull-left">
+	      		<input type="submit" name="submit" id="page_submit" class="btn btn-primary" value="Save" />
+	      		<a href="#" class="btn btn-default">Close</a>
+	      	</div>
+	</form>
+</div>
