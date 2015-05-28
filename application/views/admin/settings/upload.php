ee613aa1ad0084228d4e33e99d3a64affdeb5da8
Tachi <zaveruha007@gmail.com>
2015-05-28 23:30:34 +0300

    File Uploading

--- application/views/admin/settings/upload.php
+++ application/views/admin/settings/upload.php
@@ -19,7 +19,6 @@
 	</li>
 </ol>
 <?php echo "<p class='alert alert-dismissable alert-danger'>$error</p>"; ?>
-
 <?php echo form_open_multipart('admin/settings/upload');?>
 
 <input type="file" name="userfile" size="20" />
