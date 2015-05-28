bc72e5db15f5766dc0d3b94dc1e0bf0c63426369
Tachi <zaveruha007@gmail.com>
2015-05-28 17:25:32 +0300

    Settings MVC

--- application/controllers/admin/Settings.php
+++ application/controllers/admin/Settings.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Settings extends TCSM_controller{
+class Settings extends TCMS_controller{
 	public function index(){
 		//Load View
 		$data['main_content'] = 'admin/settings/index';
