eb788af79b1e58fd9864b6c5303e0439343ce86b
Tachi <zaveruha007@gmail.com>
2015-05-29 02:39:07 +0300

    Authorization

--- application/controllers/admin/login.php
+++ application/controllers/admin/login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends TCMS_Controller{
-	public function index(){
+	public function login(){
 		$this->load->view('admin/layouts/login');
 	}
 }
