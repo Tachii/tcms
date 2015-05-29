5794de1e9e424a400c2cc8082939054c736dec11
Tachi <zaveruha007@gmail.com>
2015-05-29 18:33:02 +0300

    Authorization

--- application/controllers/admin/Authenticate.php
+++ application/controllers/admin/Authenticate.php
@@ -1,47 +1,46 @@
-<?php 
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Authenticate extends TCMS_Controller{
+		
 	public function login(){
 		$this->form_validation->set_rules('username','Username','trim|required|min_length[4]|xss_clean');
 		$this->form_validation->set_rules('password','Password','trim|required|min_length[4]|xss_clean');
-		if ($this->form_validation->run() == FALSE) {
+		
+		if($this->form_validation->run() == FALSE){
 			//Load View
 			$this->load->view('admin/layouts/login');
 		} else {
-			//Get From Post
+			//Get Form Data
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
 			
 			//Validate Username & Password
-			$user_id = $this->Authenticate_model->login($username, $password);
+			$user= $this->Authenticate_model->login($username,$password);
 			
-			if($user_id){
+			if($user){
 				$user_data = array(
-						'user_id'   => $user_id,
-						'username'  => $username,
-						'logged_in' => true
+					'user_id' => $user->id,
+					'username' => $username,
+					'logged_in' => true
 				);
 				//Set session userdata
 				$this->session->set_userdata($user_data);
 				
-				//Set message
-				$this->session->set_flashdata('pass_login', 'You are now logged in');
+				//Set message 
+				$this->session->set_flashdata('pass_login','You are now logged in');
 				redirect('admin/dashboard');
 			}
 		}
+		
 	}
 	
-	/*
-	 * Logout User
-	 */
-	public function logout() {
-		//Unset user data
-		$this->session->unset_userdata('logged_in');
+	public function logout(){
+		//Unset User Data
 		$this->session->unset_userdata('user_id');
 		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('logged_in');
 		$this->session->sess_destroy();
-	
-		//Set message
-		$this->session->set_flashdata('logged_out', 'You have been logged out');
-		redirect('admin/login');
+		
+		redirect('admin/authenticate/login');
 	}
 }
