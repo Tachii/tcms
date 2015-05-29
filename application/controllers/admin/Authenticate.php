7ab25d4895725105092b41cf6ae98a70073fa79d
Tachi <zaveruha007@gmail.com>
2015-05-29 18:30:14 +0300

    Authorization

--- application/controllers/admin/Authenticate.php
+++ application/controllers/admin/Authenticate.php
@@ -1,46 +1,47 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Authenticate extends TCMS_Controller{
-		
+<?php 
+class Authenticate extends MY_Controller{
 	public function login(){
 		$this->form_validation->set_rules('username','Username','trim|required|min_length[4]|xss_clean');
 		$this->form_validation->set_rules('password','Password','trim|required|min_length[4]|xss_clean');
-		
-		if($this->form_validation->run() == FALSE){
+		if ($this->form_validation->run() == FALSE) {
 			//Load View
 			$this->load->view('admin/layouts/login');
 		} else {
-			//Get Form Data
+			//Get From Post
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
 			
 			//Validate Username & Password
-			$user= $this->Authenticate_model->login($username,$password);
+			$user_id = $this->Authenticate_model->login_user($username, $password);
 			
-			if($user){
+			if($user_id){
 				$user_data = array(
-					'user_id' => $user->id,
-					'username' => $user->username,
-					'logged_in' => true
+						'user_id'   => $user_id,
+						'username'  => $username,
+						'logged_in' => true
 				);
 				//Set session userdata
 				$this->session->set_userdata($user_data);
 				
-				//Set message 
-				$this->session->set_flashdata('pass_login','You are now logged in');
+				//Set message
+				$this->session->set_flashdata('pass_login', 'You are now logged in');
 				redirect('admin/dashboard');
 			}
 		}
-		
 	}
 	
-	public function logout(){
-		//Unset User Data
+	/*
+	 * Logout User
+	 */
+	public function logout() {
+		//Unset user data
+		$this->session->unset_userdata('logged_in');
 		$this->session->unset_userdata('user_id');
 		$this->session->unset_userdata('username');
-		$this->session->unset_userdata('logged_in');
 		$this->session->sess_destroy();
-		
-		redirect('admin/authenticate/login');
+	
+		//Set message
+		$this->session->set_flashdata('logged_out', 'You have been logged out');
+		redirect('admin/login');
 	}
 }
