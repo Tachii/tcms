78b24c63a3e659204db58dace0e11b58e0e55303
Tachi <zaveruha007@gmail.com>
2015-05-29 20:27:53 +0300

    Authorization

--- application/controllers/admin/Groups.php
+++ application/controllers/admin/Groups.php
@@ -1,6 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Groups extends TCMS_Controller{
+	public function __construct(){
+		parent::__construct();
+		if( ! $this->session->userdata('logged_in')){
+			redirect('admin/authenticate/login');
+		}
+		//Loop to get all settings in the "globals" table
+		foreach($this->Settings_model->get_global_settings() as $result){
+			$this->global_data[$result->key] = $result->value;
+		}
+	}
+	
 	public function index(){
 		//Get Categoires
 		$data['groups'] = $this->Group_model->get_groups();
--- application/controllers/admin/Settings.php
+++ application/controllers/admin/Settings.php
@@ -2,6 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Settings extends TCMS_controller{
+	public function __construct(){
+		parent::__construct();
+		if( ! $this->session->userdata('logged_in')){
+			redirect('admin/authenticate/login');
+		}
+		//Loop to get all settings in the "globals" table
+		foreach($this->Settings_model->get_global_settings() as $result){
+			$this->global_data[$result->key] = $result->value;
+		}
+	}
+	
 	public function index(){
 		//Load View
 		$data['settings'] = $this->Settings_model->get_global_settings();
