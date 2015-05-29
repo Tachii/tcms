42f6a098a7325974a3d541100ea10549571bcc77
Tachi <zaveruha007@gmail.com>
2015-05-29 19:40:35 +0300

    Authorization

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Articles';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['admin'] = "admin/dashboard";
+$route['admin/login'] = "admin/authenticate/login";
+$route['admin/logout'] = "admin/authenticate/logout";
