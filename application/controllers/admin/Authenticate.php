d1bc16aa4613cea9d02fd280b7ff0d91cb125360
Tachi <zaveruha007@gmail.com>
2015-05-29 16:30:00 +0300

    Authorization

--- application/controllers/admin/Authenticate.php
+++ application/controllers/admin/Authenticate.php
@@ -4,10 +4,14 @@ class Authenticate extends TCMS_Controller{
 	public function login(){
 		$this->form_validation->set_rules('username','Username','trim|required|min_length[4]|xss_clean');
 		$this->form_validation->set_rules('password','Username','trim|required|min_length[4]|xss_clean');
+		
 		if($this->form_validation->run()== FALSE) {
+		
 			//Loading View
 			$this->load->view('admin/layouts/login');
+		
 		} else {
+		
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
 			
@@ -26,7 +30,8 @@ class Authenticate extends TCMS_Controller{
 				
 				//Set message
 				$this->session->set_flashdata('pass_login', 'You are now logged in');
-				redirect('admin/dashboard');
+				$this->load->view('admin/dashboard');
+		
 			}
 		}
 	}
