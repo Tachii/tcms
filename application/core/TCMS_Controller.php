517bacaf4b0680d0d209cc57889a2c64f762c470
Tachi <zaveruha007@gmail.com>
2015-05-29 19:26:36 +0300

    Authorization

--- application/core/TCMS_Controller.php
+++ application/core/TCMS_Controller.php
@@ -12,3 +12,14 @@ class TCMS_Controller extends  CI_Controller{
 		}
 	}
 }
+
+class INDEX_Controller extends  CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		//Loop to get all settings in the "globals" table
+		foreach($this->Settings_model->get_global_settings() as $result){
+			$this->global_data[$result->key] = $result->value;
+		}
+	}
+}
+
