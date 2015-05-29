cb8af7ef95b8cc9a8e7282d2a5d8c4fe5130b429
Tachi <zaveruha007@gmail.com>
2015-05-29 19:44:08 +0300

    Authorization

--- application/core/TCMS_Controller.php
+++ application/core/TCMS_Controller.php
@@ -4,7 +4,7 @@ class TCMS_Controller extends  CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		if( ! $this->session->userdata('logged_in')){
-			redirect('admin/login');
+			redirect('admin/authenticate/login');
 		}
 		//Loop to get all settings in the "globals" table
 		foreach($this->Settings_model->get_global_settings() as $result){
