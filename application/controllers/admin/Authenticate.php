d799b28abfaf1313262a06b42b4b5945af36bcb8
Tachi <zaveruha007@gmail.com>
2015-05-29 18:09:13 +0300

    Authorization

--- application/controllers/admin/Authenticate.php
+++ application/controllers/admin/Authenticate.php
@@ -1,38 +1,37 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Authenticate extends TCMS_Controller{
+		
 	public function login(){
 		$this->form_validation->set_rules('username','Username','trim|required|min_length[4]|xss_clean');
 		$this->form_validation->set_rules('password','Username','trim|required|min_length[4]|xss_clean');
 		
-		if($this->input->post('submit')) {
-			if($this->form_validation->run() == FALSE){
-				//Loading View
-				$this->load->view('admin/layouts/login');
-				
-			
-				$username = $this->input->post('username');
-				$password = $this->input->post('password');
-				
-				//Validate Username & Password
-				$user_id = $this->Authenticate_model->login($username, $password);
-				
-				if($user_id){
-					$user_data = array(
-						'user_id' => $user_id,
-						'username' => $username,
-						'logged_in' => true
-					);
-				
-					//Set session userdata
-					$this->session->set_userdata($user_data);
-				}
-			} 
+		if($this->form_validation->run() == FALSE){
+			//Load View
+			$this->load->view('admin/layouts/login');
 		} else {
-				//Set message
-				$this->session->set_flashdata('pass_login', 'You are now logged in');
+			//Get Form Data
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			
+			//Validate Username & Password
+			$user_id=$this->Authenticate_model->login($username,$password);
+			
+			if($user_id){
+				$user_data = array(
+					'user_id' => $user_id,
+					'username' => $username,
+					'logged_in' => true
+				);
+				//Set session userdata
+				$this->session->set_userdata($user_data);
+				
+				//Set message 
+				$this->session->set_flashdata('pass_login','You are now logged in');
 				redirect('admin/dashboard');
+			}
 		}
+		
 	}
 	
 	public function logout(){
